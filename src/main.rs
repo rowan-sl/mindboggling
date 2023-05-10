e58b2515c94d22c1aa1186f1ec2cae6696782986
rowan <r@fawkes.io>
2023-05-10 18:17:02 -0400

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,174 @@
-use packed_simd::{cptrx8, isizex8, m8x8, msizex8, u64x8, usizex8, Cast};
-use std::{cell::UnsafeCell, hint::black_box, mem, time::Instant};
-
-const WORDLIST: &str = include_str!("../assets/collins_srabble_words_2019.txt");
+use std::collections::HashSet;
 
 fn main() {
-    let board = Board::from_str("e o l o e e t o o o y k f d e qu f i s e o v e e s").unwrap();
+    //TODO: properly handle QU
+    let board = Board::from_str(
+        "e o l o e e t o o o y k f d e qu f i s e o v e e s"
+            .to_ascii_lowercase()
+            .as_str(),
+    )
+    .unwrap();
 
     println!("Board:\n{}", board.display_str());
 
-    let list_raw = WORDLIST
-        .split("\n\r")
-        .skip(2) // title section of list
-        .map(|word| {
-            assert!(
-                word.chars().map(|c| c.is_ascii_alphabetic()).all(|x| x),
-                "{word:?}"
-            );
-            word.to_string()
+    let list_raw = std::fs::read_to_string(std::env::var("WORDPATH").unwrap()).unwrap();
+
+    // parsing for https://ucrel.lancs.ac.uk/bncfreq/flists.html (list 1.1 complete no cut off)
+    let list_vec = list_raw
+        .split('\n')
+        .skip(1)
+        .filter_map(|line| {
+            if line.is_empty() {
+                return None;
+            }
+            println!("line {line:?}");
+            let [
+            // the word
+            ref word @ ..,
+            // part of speech
+            part_of_speech,
+            // variant
+            // if ":", it is just a normal word,
+            // if "%", it is what appears to be a variation,
+            // if <other> it is a variation of the last one with "%" as its tag.
+            //     note that for variations, `word` and `part_of_speech` are "@"
+            variant,
+            // rounded frequency per million word tokens (down to min of 10)
+            freq,
+            // number of sectors of the corpus (max 100) in which the word occurs
+            range,
+            // Dispersion value (Juilland's D) from a minimum of 0.00 to a maximum of 1.00
+            dispersion
+        ] = line.split('\t').collect::<Vec<_>>()[..] else { panic!("bad formatting") };
+            let word = word.iter().copied().collect::<String>();
+            let freq: u64 = freq.parse().unwrap();
+            let range: u64 = range.parse().unwrap();
+            let dispersion: f64 = dispersion.parse().unwrap();
+            // -- filters --
+            
+            Some((word, part_of_speech, variant, freq, range, dispersion))
         })
-        .collect::<Vec<_>>();
-    let list = WordPart::from_collection(&list_raw);
+        .filter_map(|(word, _, _, _, _, _)| {
+            if word != "@" && word.chars().all(|c| c.is_ascii_alphabetic()) {
+                Some(word)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<String>>();
+
+    // let list_vec = list_raw
+    //     .to_ascii_lowercase() // IMPORTANT
+    //     .split("\n")
+    //     .skip(2) // title section of list
+    //     .map(|word| {
+    //         let word = word.trim();
+    //         assert!(
+    //             word.chars().map(|c| c.is_ascii_alphabetic()).all(|x| x),
+    //             "{word:?}"
+    //         );
+    //         word.to_string()
+    //     })
+    //     .collect::<Vec<_>>();
+
+    let list = WordPart::from_collection(&list_vec, false);
+
+    #[allow(unused)]
+    fn dbg_wl(part: &'static WordPart, prev: String) {
+        for (i, next) in part.next[..=26].iter().enumerate() {
+            if let Some(next) = next {
+                let s = prev.clone() + Tile(i).to_ch().unwrap().to_string().as_str();
+                if next.completes_word {
+                    println!("word {s}");
+                }
+                dbg_wl(*next, s);
+            }
+        }
+    }
+    //dbg_wl(list, "".into());
 
     println!("Created word list");
 
-    unsafe {
-        for i in 0..10_000 {
-            black_box((
-                //asdf(&board, list, (2, 2)),
-                asdf_nosimd(&board, list, (2, 2)),
-            ));
+    let mut found = HashSet::new();
+
+    for x in 1..=BOARD_SIZE {
+        for y in 1..=BOARD_SIZE {
+            println!("run for ({x}, {y})");
+            fn iter(
+                x: usize,
+                y: usize,
+                board: Board,
+                list: &'static WordPart,
+                previous: String,
+                previous_coords: Vec<(usize, usize)>,
+                n: usize,
+                found: &mut HashSet<String>,
+            ) {
+                let indent = std::iter::repeat("  ").take(n).collect::<String>();
+                println!(
+                    "{indent}  iter x {x} y {y} prev {previous:?} valid next {:?}",
+                    list.next[..=26]
+                        .iter()
+                        .enumerate()
+                        .filter_map(|(i, x)| {
+                            (*x)?;
+                            Some(Tile(i).to_ch().unwrap())
+                        })
+                        .collect::<String>()
+                );
+                let (tiles, parts) = unsafe { asdf_nosimd(&board, list, (x, y)) };
+                for (i, (tile, part)) in tiles.iter().zip(parts.iter()).enumerate() {
+                    if *tile == Tile::invalid() {
+                        println!("{indent}    tile #{i} INVALID");
+                        continue;
+                    }
+                    println!("{indent}    tile #{i} char '{}'", tile.to_ch().unwrap());
+                    let this = previous.clone() + tile.to_ch().unwrap().to_string().as_str();
+                    let x2 = x
+                        .checked_add_signed(match i {
+                            0 | 3 | 5 => -1,
+                            1 | 6 => 0,
+                            2 | 4 | 7 => 1,
+                            _ => unreachable!(),
+                        })
+                        .unwrap();
+                    let y2 = y
+                        .checked_add_signed(match i {
+                            0 | 1 | 2 => -1,
+                            3 | 4 => 0,
+                            5 | 6 | 7 => 1,
+                            _ => unreachable!(),
+                        })
+                        .unwrap();
+                    let mut board2 = board.clone();
+                    board2.0[x2 + y2 * BOARD_SIZE_PADDED] = Tile::invalid();
+                    let mut previous_coords2 = previous_coords.clone();
+                    previous_coords2.push((x2, y2));
+                    if let Some(part) = part {
+                        if part.completes_word {
+                            println!("{indent}      found word {this:?} path {previous_coords2:?}");
+                            found.insert(this.clone());
+                        }
+                        iter(x2, y2, board2, part, this, previous_coords2, n + 2, found);
+                    }
+                }
+            }
+            let at = board.0[x + y * BOARD_SIZE_PADDED];
+            if let Some(list_at) = list.next[at.to_idx()] {
+                iter(
+                    x,
+                    y,
+                    board.clone(),
+                    list_at,
+                    at.to_ch().unwrap().to_string(),
+                    vec![(x, y)],
+                    0,
+                    &mut found,
+                );
+            }
         }
-        let before1 = Instant::now();
-        // for i in 0..10_000 {
-        //     let next = asdf(&board, list, (2, 2));
-        //     black_box(next);
-        // }
-        let time1 = before1.elapsed();
-        let before2 = Instant::now();
-        for i in 0..10_000 {
-            let next2 = asdf_nosimd(&board, list, (2, 2));
-            black_box(next2);
-        }
-        let time2 = before2.elapsed();
-        let before3 = Instant::now();
-        for i in 0..10_000 {
-            let next3 = asdf2(&board, list, (2, 2));
-            black_box(next3);
-        }
-        let time3 = before3.elapsed();
-        println!("simd: {time1:?}\nno simd: {time2:?}\nnew simd:{time3:?}");
-        //assert_eq!(next.0, next2.0);
-        // let mut b = board;
-        // let mut word: Vec<char> = vec![];
-        // loop {
-        //     b.0[2*BOARD_SIZE_PADDED+2+1] = Tile(usize::MAX);
-        //     // next = asdf(&b, *list.1[next_idx], next_coord);
-        // }
-        // println!("{:?}", next.0.map(|i| i.to_ch()));
     }
+    println!("found {found:?}");
 }
 
 //#[no_mangle]
@@ -83,101 +194,6 @@ pub unsafe fn asdf_nosimd(
         words[n] = *cwl.as_slice().get(tile.to_idx()).unwrap_unchecked();
     }
     (tiles, words)
-}
-
-#[no_mangle]
-pub unsafe fn asdf2(
-    board: &Board,
-    cwl: &'static WordPart,
-    idx: (usize, usize),
-) -> ([Tile; 8], [Option<&'static WordPart>; 8]) {
-    const B: isize = BOARD_SIZE_PADDED as isize;
-    const OFFSETS: isizex8 = isizex8::new(-B - 1, -B, -B + 1, -1, 1, B - 1, B, B + 1);
-    debug_assert!(
-        idx.0 > 0 && idx.0 < BOARD_SIZE_PADDED && idx.1 > 0 && idx.1 < BOARD_SIZE_PADDED,
-        "out of range!"
-    );
-    let m = msizex8::splat(true);
-    let z = u64x8::splat(0);
-    let tiles = cptrx8::splat(
-        board
-            .0
-            .as_ptr()
-            .add(idx.0 + idx.1 * BOARD_SIZE_PADDED)
-            .cast(),
-    )
-    .offset(OFFSETS)
-    .read(m, z);
-
-    // let mut tiles = [0u64; 8];
-    // let tiles_ptr = i.read(msizex8::splat(true), u64x8::splat(0));
-    // tiles_ptr.write_to_slice_unaligned_unchecked(&mut tiles);
-
-    let words = cptrx8::splat(cwl.as_slice().as_ptr().cast())
-        .add(tiles.cast())
-        .read(m, z);
-
-    (mem::transmute(tiles), mem::transmute(words))
-    // let mut tiles = [Tile::invalid(); 8];
-    // let mut words = [None; 8];
-    // for (n, i) in OFFSETS.into_iter().enumerate() {
-    //     let i = (idx.0 + idx.1 * BOARD_SIZE_PADDED).checked_add_signed(i).unwrap_unchecked();
-    //     let tile = board.0[i];
-    //     tiles[n] = tile;
-    //     words[n] = *cwl.as_slice().get(tile.to_idx()).unwrap_unchecked();
-    // }
-    // (tiles, words)
-}
-
-//#[no_mangle]
-pub unsafe fn asdf(
-    board: &Board,
-    cwl: &WordPart,
-    idx: (usize, usize),
-) -> ([Tile; 8], [*mut WordPart; 8]) {
-    const B: isize = BOARD_SIZE_PADDED as isize;
-    const OFFSETS: isizex8 = isizex8::new(-B - 1, -B, -B + 1, -1, 1, B - 1, B, B + 1);
-    debug_assert!(
-        idx.0 > 0 && idx.0 < BOARD_SIZE_PADDED && idx.1 > 0 && idx.1 < BOARD_SIZE_PADDED,
-        "out of range!"
-    );
-    // if an idx is off the side of the board, it will fall in the gutter area (Tiles set to usize::MAX)
-    // this can be detected later
-    let idxs: usizex8 =
-        (isizex8::splat((idx.0 + idx.1 * BOARD_SIZE_PADDED) as isize) + OFFSETS).cast();
-
-    #[inline]
-    unsafe fn gather_or(from: &[usize], idxs: usizex8, or: usize) -> usizex8 {
-        //saftey check
-        //assert_eq!(mem::size_of::<usize>(), mem::size_of::<u64>());
-        //this does not work (the cptrx8::read) with usize elements (https://github.com/rust-lang/packed_simd/issues/237)
-        let base = from.as_ptr().cast::<u64>();
-        let mask: msizex8 = idxs.lt(usizex8::splat(from.len()));
-        let alt = u64x8::splat(mem::transmute::<usize, u64>(or));
-        cptrx8::splat(base).add(idxs).read(mask, alt).cast()
-    }
-
-    #[inline]
-    unsafe fn to_array(val: usizex8) -> [usize; 8] {
-        let mut slice = [0usize; 8];
-        val.write_to_slice_unaligned_unchecked(&mut slice);
-        slice
-    }
-
-    let letter_idxs = gather_or(
-        mem::transmute::<&[Tile], &[usize]>(&board.0),
-        idxs,
-        usize::MAX,
-    );
-    let next_letters = gather_or(
-        mem::transmute::<&[Option<&'static WordPart>], &[usize]>(cwl.as_slice()),
-        letter_idxs,
-        0,
-    );
-    (
-        mem::transmute(to_array(letter_idxs)),
-        mem::transmute(to_array(next_letters)),
-    )
 }
 
 #[repr(transparent)]
@@ -271,11 +287,12 @@ fn ch_to_idx_correct_start() {
     assert_eq!(Tile::from_char('a'), Some(Tile(0)));
 }
 
-#[repr(transparent)]
 #[derive(Debug)]
-pub struct WordPart(
-    [Option<&'static WordPart>; 27], /* 27th entry is for out-of-bounds, allways None*/
-);
+pub struct WordPart {
+    next: [Option<&'static WordPart>; 27], /* 27th entry is for out-of-bounds, allways None*/
+    // if this completes a word
+    completes_word: bool,
+}
 // #[derive(Debug)]
 // pub enum WordPart {
 //     More(Box<[WordPart; 26]>),
@@ -283,24 +300,32 @@ pub struct WordPart(
 // }
 
 impl WordPart {
-    pub fn from_collection(words: &[String]) -> &'static Self {
-        let mut starting_with: [Vec<String>; 27] = Default::default();
+    pub fn from_collection(words: &[String], completes_word: bool) -> &'static Self {
+        let mut starting_with: [(Vec<String>, bool); 27] = Default::default();
         for word in words {
             let mut iter = word.chars();
-            if let Some(c) = iter.nth(0) {
-                starting_with[Tile::from_char(c).unwrap().to_idx()].push(iter.collect())
+            if let Some(c) = iter.next() {
+                starting_with[Tile::from_char(c).unwrap().to_idx()]
+                    .0
+                    .push(iter.collect());
+                if word.len() == 1 {
+                    starting_with[Tile::from_char(c).unwrap().to_idx()].1 |= true;
+                }
             }
         }
-        Box::leak(Box::new(Self(starting_with.map(|e| {
-            if e.is_empty() {
-                None
-            } else {
-                Some(Self::from_collection(&e).into())
-            }
-        }))))
+        Box::leak(Box::new(Self {
+            next: starting_with.map(|e| {
+                if e.0.is_empty() {
+                    None
+                } else {
+                    Some(Self::from_collection(&e.0, e.1))
+                }
+            }),
+            completes_word,
+        }))
     }
 
     pub fn as_slice(&self) -> &[Option<&'static Self>] {
-        &self.0
+        &self.next
     }
 }
